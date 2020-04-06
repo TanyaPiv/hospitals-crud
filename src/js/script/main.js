ac6e84e26f7caf8b950472dc7f28d9cac449321a
Tatyana Pivgorodnyaya <peachpeachttt@gmail.com>
2020-04-06 11:47:12 +0300

    finished project

--- src/js/script/main.js
+++ src/js/script/main.js
@@ -1,3 +1,36 @@
+if (!Array.prototype.find) {
+    Array.prototype.find = function (predicate) {
+        if (this == null) {
+            throw new TypeError('Array.prototype.find called on null or undefined');
+        }
+        if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+        }
+        var list = Object(this);
+        var length = list.length >>> 0;
+        var thisArg = arguments[1];
+        var value;
+
+        for (var i = 0; i < length; i++) {
+            value = list[i];
+            if (predicate.call(thisArg, value, i, list)) {
+                return value;
+            }
+        }
+        return undefined;
+    };
+}
+
+(function (ELEMENT) {
+    ELEMENT.matches = ELEMENT.matches || ELEMENT.mozMatchesSelector || ELEMENT.msMatchesSelector || ELEMENT.oMatchesSelector || ELEMENT.webkitMatchesSelector;
+    ELEMENT.closest = ELEMENT.closest || function closest(selector) {
+        if (!this) return null;
+        if (this.matches(selector)) return this;
+        if (!this.parentElement) { return null }
+        else return this.parentElement.closest(selector)
+    };
+}(Element.prototype));
+
 (function () {
     var arr = [window.Element, window.CharacterData, window.DocumentType];
     var args = [];
@@ -39,6 +72,7 @@ window.onload = () => {
     }
     addButtonClickHandler();
     deleteHospital();
+    editHospital();
 }
 
 const createId = () => {
@@ -105,10 +139,19 @@ const modalCloseHandler = () => {
     modal.classList.remove('modal_active');
 }
 
-const modalOpenHandler = () => {
+const modalOpenHandler = (type) => {
     const modal = document.querySelector('.modal');
     const buttonClose = document.querySelector('.modal-close');
     const form = document.querySelector('form');
+    const button = document.querySelector('.modal-submit')
+
+    if (!type && button.innerHTML !== 'Добавить') {
+        button.innerHTML = 'Добавить';
+    }
+
+    if (type && type === 'edit' && button.innerHTML !== 'Изменить' ) {
+        button.innerHTML = 'Изменить';
+    }
 
     buttonClose.onclick = function (event) {
         event.preventDefault();
@@ -122,8 +165,10 @@ const modalOpenHandler = () => {
 const addButtonClickHandler = () => {
     const button = document.querySelector('.button-add');
 
-    button.onclick = modalOpenHandler;
-    addHospital();
+    button.onclick = () => {
+        modalOpenHandler();
+        addHospital();
+    }
 }
 
 const addHospital = () => {
@@ -182,13 +227,14 @@ const addHospital = () => {
         form.reset();
         modalCloseHandler();
         deleteHospital();
+        editHospital();
     }
 
 
 }
 
 const deleteHospital = () => {
-    const buttons = document.querySelectorAll('.delete');
+    const buttons = Array.prototype.slice.call(document.querySelectorAll('.delete'));
 
     buttons.forEach((button) => {
         button.onclick = function () {
@@ -219,6 +265,71 @@ const renderNoData = () => {
 }
 
 const editHospital = () => {
-    const buttons = document.querySelectorAll('.edit');
+    const buttons = Array.prototype.slice.call(document.querySelectorAll('.edit'));
+
+    buttons.forEach((button) => {
+        button.onclick = function () {
+            const buttonSub = document.querySelector('.modal-submit');
+            const inputs = Array.prototype.slice.call(document.querySelectorAll('input'))
+            const parent = this.closest('.table-item');
+            const form = document.querySelector('form');
+            let empty = false;
+            const id = parent.getAttribute('data-id');
+            const selectedHospital = hospitals.find((el)=> {
+                return el.id === id;
+            })
+
+
+            inputs[0].value = selectedHospital.full_name;
+            inputs[1].value = selectedHospital.address;
+            inputs[2].value = selectedHospital.phone;
+
+            modalOpenHandler('edit');
+
+            buttonSub.onclick = function (event) {
+                event.preventDefault();
+
+                inputs.forEach((input) => {
+                    if (input.value.length < 1) {
+                        empty = true;
+                        return;
+                    }
+                })
+
+                if (empty) {
+                    empty = false;
+                    return;
+                }
+
+                hospitals = hospitals.map((el)=> {
+                    if (el.id === id) {
+                        return {
+                            ...el, 
+                            full_name: inputs[0].value,
+                            address: inputs[1].value,
+                            phone: inputs[2].value
+                        }
+                    }
+                    return {...el}
+                })
+
+                const json = JSON.stringify(hospitals)
+                localStorage.setItem('hospitals', json)
+
+                let children = parent.querySelectorAll('div')
+                children = Array.prototype.slice.call(children)
+                children = children.filter((el) => {
+                    return !el.classList.contains('table-item__controls')
+                })
+
+                children.forEach((child, index) => {
+                    child.innerHTML = inputs[index].value;
+                });
+
+                modalCloseHandler();
+                form.reset();
+            }
+        }
+    })
     
 }
